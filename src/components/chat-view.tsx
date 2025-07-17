ee1d6a494efbc7a570962c20a44e87855e1a8bb1
koki330 <sakurai.koki@tecnos.co.jp>
2025-07-17 19:44:21 +0900

    チャット履歴とプロンプト入力欄の分離

--- src/components/chat-view.tsx
+++ src/components/chat-view.tsx
@@ -14,8 +14,8 @@ const ChatView = () => {
   }, [messages]);
 
   return (
-    <div className="flex-1 flex flex-col p-4 overflow-y-auto">
-      <div className="flex-1 space-y-4">
+    <div className="flex-1 flex flex-col overflow-hidden">
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
         {messages.length === 0 ? (
           <p className="text-center text-gray-500">Select a model and start chatting.</p>
         ) : (
